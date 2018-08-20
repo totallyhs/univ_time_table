6f551275d2c511f3c1451efb3bd1e41970028d86
hyunseung <hyunseung@192.168.10.54>
2018-08-20 20:05:16 +0900

    PostlistVo

---
+++ src/main/java/org/timetable/univ/model/vo/PostlikeVo.java
@@ -0,0 +1,21 @@
+package org.timetable.univ.model.vo;
+
+public class PostlikeVo {
+	private int postNo;
+	private String name;
+	
+	public int getPostNo() {
+		return postNo;
+	}
+	public void setPostNo(int postNo) {
+		this.postNo = postNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}

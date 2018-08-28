b10a6807e20cd1d8eb1496dcb21f573649f50fc2
HeeSang <HeeSang@192.168.200.144>
2018-08-28 21:37:49 +0900

    mail

--- src/main/java/org/timetable/univ/tiles/controller/MailController.java
+++ src/main/java/org/timetable/univ/tiles/controller/MailController.java
@@ -5,13 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/mail")
 public class MailController {
 
 	@RequestMapping("/inbox")
-	public ModelAndView inboxHandle() {
+	public ModelAndView inboxHandle(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 		
-		mav.setViewName("inbox");
+		mav.setViewName("mail.inbox");
+		mav.addObject("uri", request.getRequestURI());
+		return mav;
+	}
+	
+	@RequestMapping("/compose")
+	public ModelAndView composingHandle(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("mail.compose");
+		mav.addObject("uri", request.getRequestURI());
 		return mav;
 	}
 }

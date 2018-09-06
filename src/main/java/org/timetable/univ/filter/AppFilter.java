460574ee64b7cfee6e172d65ea1a8efe4e566bb4
dbflt <dbflt@192.168.35.197>
2018-09-06 11:08:29 +0900

    .

--- src/main/java/org/timetable/univ/filter/AppFilter.java
+++ src/main/java/org/timetable/univ/filter/AppFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
